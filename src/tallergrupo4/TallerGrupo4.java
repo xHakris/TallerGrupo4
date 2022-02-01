65fb5c12081d259741851ededb0abb5183d0c764
Diego <pinchadiego8@gmail.com>
2022-02-01 14:55:53 -0500

    Mensaje por consola

--- src/tallergrupo4/TallerGrupo4.java
+++ src/tallergrupo4/TallerGrupo4.java
@@ -16,6 +16,7 @@ public class TallerGrupo4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Taller en grupo");
     }
     
 }
